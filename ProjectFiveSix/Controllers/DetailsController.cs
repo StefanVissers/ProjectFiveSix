1bcecc6088b83de7dabec46d19c31be80bb42545
StefanVissers <0906707@hr.nl>
2016-11-02 10:51:39 +0100

    Second Commit

---
+++ ProjectFiveSix/Controllers/DetailsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectFiveSix.Models;
+
+namespace ProjectFiveSix.Controllers
+{
+    public class DetailsController : Controller
+    {
+        // GET: Details
+        public ActionResult Index(String category)
+        {
+            var categoryModel = new Category { Name = category };
+            return View(categoryModel);
+        }
+    }
+}
---
+++ ProjectFiveSix/Controllers/StoreController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectFiveSix.Models;
+
+namespace ProjectFiveSix.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            var categories = new List<Category>
+            {
+                new Category { Name = "Processor" },
+                new Category { Name = "Graphics Card" },
+                new Category { Name = "Motherboard" },
+                new Category { Name = "Hard Drives" },
+                new Category { Name = "Random Access Memory" },
+                new Category { Name = "Sound Systems" },
+                new Category { Name = "Monitor" }
+            };
+            return View(categories);
+        }
+        public ActionResult Browse(String category)
+        {
+            var categoryModel = new Category { Name = category };
+            return View(categoryModel);
+        }
+    }
+}
