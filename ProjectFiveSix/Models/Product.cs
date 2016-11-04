9542f933b14bc2818e4f6045ef8199768b211ae2
StefanVissers <0906707@hr.nl>
2016-11-04 10:53:22 +0100

    LocalDB TODO naar onlineDB

---
+++ ProjectFiveSix/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace ProjectFiveSix.Models
+{
+    public class Product
+    {
+        public int productID { get; set; }
+        public String productName { get; set; }
+        public String manufactorer { get; set; }
+        public String category { get; set; }
+        public decimal price { get; set; }
+    }
+    public class ProductDBContext : DbContext
+    {
+        public DbSet<Product> products { get; set; }
+    }
+}
