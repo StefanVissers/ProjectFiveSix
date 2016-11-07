19e837cd961995b0050dcc7140047d802602aa11
Thomas de Graaf <deraaf4@hotmail.com>
2016-11-07 13:27:28 +0100

    Models

---
+++ ProjectFiveSix/Models/admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectFiveSix.Models
+{
+    public class admin
+    {
+        public int admin_id { get; set; }
+        public String username { get; set; }
+        public String password { get; set; }
+    }
+}
---
+++ ProjectFiveSix/Models/order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectFiveSix.Models
+{
+    public class order
+    {
+        public int order_id { get; set; }
+        public int customer_id { get; set; }
+        public String status { get; set; }
+
+    }
+}
